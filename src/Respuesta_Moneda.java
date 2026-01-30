359c57574e28a3b573f9f85295ce22d9e8d705dc
luis fernando <thefernando.lol.27@gmail.com>
2026-01-30 00:08:46 -0600

    Modifica la clase Respuesta Moneda

--- src/Respuesta_Moneda.java
+++ src/Respuesta_Moneda.java
@@ -1,2 +1,13 @@
+import java.util.Map;
+
 public class Respuesta_Moneda {
+    private String codigo;
+    private double cantidad;
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public Map<String, double> datosDeConversion = new Map<String, double>() {
+    }
 }
