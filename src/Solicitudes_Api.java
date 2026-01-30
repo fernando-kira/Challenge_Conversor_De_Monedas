f6373abc012bab0a2b18745d2f8dff26370c29de
luis fernando <thefernando.lol.27@gmail.com>
2026-01-30 00:17:54 -0600

    Modifica solicutdes_Api

--- src/Solicitudes_Api.java
+++ src/Solicitudes_Api.java
@@ -5,6 +5,10 @@ import java.net.http.HttpResponse;
 
 public class Solicitudes_Api {
 
+    public void solicitaMoneda (Respuesta_Moneda respuestaMoneda){
+        String url ="https://v6.exchangerate-api.com/v6/e1704b867986787e0bf14dd7/latest/"+
+                respuestaMoneda.getCodigo();
+    }
 
 
 }
