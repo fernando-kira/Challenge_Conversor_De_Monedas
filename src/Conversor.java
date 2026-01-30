5b4786ef50c718922da66483548bf1bf33f84712
luis fernando <thefernando.lol.27@gmail.com>
2026-01-30 01:33:50 -0600

    modifica el conversor

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,2 +1,11 @@
 public class Conversor {
+
+    public double convertir (String monedaDeseada, Double cantidad, Respuesta_Moneda respuestaMoneda){
+        monedaDeseada.toUpperCase();
+       var resultado = respuestaMoneda.getConversion_rates().get(monedaDeseada);
+       if(!respuestaMoneda.getBase_code().contains(monedaDeseada)){
+           throw new IllegalArgumentException("Moneda no encontrada");
+       }
+        return resultado *cantidad;
+    }
 }
